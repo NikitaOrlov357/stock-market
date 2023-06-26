34b35f4302bd51b694c8fd72325a97e19f2f3546
BabyBurrito <mywketer00@mail.ru>
2023-06-26 17:52:46 +0300

    init norm

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,33 +1,20 @@
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS balance;
-DROP TABLE IF EXISTS history;
-DROP TABLE IF EXISTS commission;
+DROP TABLE IF EXISTS transaction;
 
 CREATE TABLE users(
     id SERIAL NOT NULL PRIMARY KEY,--уникальный номер записи
     name VARCHAR NOT NULL UNIQUE, -- имя пользователя
-    password VARCHAR,-- init пароль
-    balance_id SERIAL NOT NULL
+    password VARCHAR-- init пароль
 );
 
-CREATE TABLE balance(
-	id INT NOT NULL REFERENCES users(balance_id),
+CREATE TABLE transaction(
+    id SERIAL NOT NULL PRIMARY KEY,
+	users_id INT NOT NULL REFERENCES users(id),
+	date DATE NOT NULL default NOW(),
 	amount INT NOT NULL,
-	currency INT NOT NULL
-);
-
-CREATE TABLE history(
-	id SERIAL NOT NULL PRIMARY KEY,
-	date DATE NOT NULL,
-	amount INT NOT NULL,
-	currency INT NOT NULL
-);
-
-CREATE TABLE commission(
-	id SERIAL NOT NULL PRIMARY KEY,
-	amount INT NOT NULL,
-	currency INT NOT NULL
+	currency INT NOT NULL,
+    commission DOUBLE PRECISION default 0
 );
 
 
-INSERT INTO users (id, password, balance_id) VALUES (1, nekit, nekit, 11)
+INSERT INTO users (name, password) VALUES ('nekit', 'nekit')
